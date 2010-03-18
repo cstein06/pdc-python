26c74e18583aa039e78a5de4d276c8b7a0eb9a92
cstein06 <cstein06@f1e8b664-948c-11dd-9015-cf692397e6c8>
2010-03-18 20:39:59 +0000

    adapted to new params.

--- trunk/pdc/src/projects/edu_estagios.py
+++ trunk/pdc/src/projects/edu_estagios.py
@@ -12,38 +12,23 @@ from scipy.linalg.basic import det
 
 import pdc.plotting as pl
 
-def window_analysis(data, analysis_function, window_size = 5, 
-                    n_frequencies = 100, sampling_rate = 500,
-                    maxp = 30, fixp = False, detrend = False, 
-                    power = False, metric = None):
+from pdc.globals import *
+
+def window_analysis(data, **args):
     
+    read_args(args)
     
     print data.shape
-    
-    
     n, T = data.shape
     
-    win = window_size*sampling_rate
-    
+    win = pr_.window_size*pr_.sample_f
     
     nwins = T/win
     
-    resp = zeros([nwins, n, n, n_frequencies])
+    resp = zeros([nwins, n, n, pr_.nf])
     
     for i in arange(nwins):
-        if metric == None:
-            aux = analysis_function(data[:,i*win:i*win+win], nf = n_frequencies, 
-                                        detrend = detrend, ss = False,
-                                        maxp = maxp, fixp = fixp)
-        else:
-            aux = analysis_function(data[:,i*win:i*win+win], nf = n_frequencies, 
-                                        detrend = detrend, ss = False,
-                                        metric = metric, maxp = maxp, fixp = fixp)
-            
-        if power:
-            aux = aux*aux.conj()
-            
-        resp[i] = aux
+        resp[i] = an_.measure(data[:,i*win:i*win+win], ss = False)
         
         print 'Processed', i+1, 'of', nwins, 'windows:', 100*(i+1.0)/nwins, '%'
     
@@ -65,21 +50,46 @@ def mean_estag(mes, estag, maxe = 6, nulle = -1):
     print npdc
     
     for i in arange(maxe):
-        mpdc[i] = mpdc[i]/npdc[i]
+        if npdc[i] > 0:            
+            mpdc[i] = mpdc[i]/npdc[i]
             
     return mpdc
         
 def main_analysis(data = None):
     
-    root = 'C:/Documents and Settings/Stein/Desktop/teste edu/'
+    root = 'C:\\Documents and Settings\\Stein\\My Documents\\dados\\teste edu\\'
         
-    
-    tim = time.clock()
-    
     #input = 'C:/Documents and Settings/Stein/Desktop/teste edu/baccala2001a_ex4_sim_01.txt'
     #input = root + 'ES57_09_02_09_medias.txt'
     #input = root + 'ES57_09_02_09_medias_curto.txt'
-    input = root + 'ES57_09_02_09_melhores.txt'
+    input = root + 'ES57_09_02_09_medias_test.txt'
+    #input = root + 'ES57_09_02_09_melhores.txt'
+    
+    inestag = root + 'ES57_09_02_09_estagiamentojanela10s_limpo.txt'
+    
+    plot_estag = array([1,2,3,4,5,6])
+
+    algoritmo = 'pdc'
+    #algoritmo = 'coh'
+    window_size = 10
+    n_frequencies = 250
+    plot_freq = 150
+    sampling_rate = 500
+    ordem_max = 25
+    ordem_fixa = True
+    detrend = True
+    espectro_em_potencia = False
+    metrica_pdc = 'diag'
+
+
+    #nao mexer a partir daqui
+
+    tim = time.clock()
+    
+    set_params(alg = algoritmo, window_size = window_size, 
+               nf = n_frequencies, sample_f = sampling_rate,
+               maxp = ordem_max, fixp = ordem_fixa, detrend = detrend, 
+               power = espectro_em_potencia, metric = metrica_pdc)
       
     if (data == None):  
         data = loadtxt(input).T
@@ -88,40 +98,26 @@ def main_analysis(data = None):
     
     print 'data loaded'
     
-    pdcan = window_analysis(data, an_.pdc, window_size = 10, 
-                            n_frequencies = 250, sampling_rate = 500,
-                            maxp = 25, fixp = True, detrend = True, 
-                            power = True, metric = 'diag')
-    
-#    pdcan = window_analysis(data, an_.coh, window_size = 10, 
-#                            n_frequencies = 250, sampling_rate = 500,
-#                            maxp = 25, fixp = True, detrend = True, 
-#                            power = False)
-    
-    inestag = root + 'ES57_09_02_09_estagiamentojanela10s_limpo.txt'
+    result = window_analysis(data)
     
     estag = loadtxt(inestag)
     
     print estag.shape
-    
-
-    mpdc = mean_estag(pdcan, estag)
+    mpdc = mean_estag(result, estag)
     
     nf = mpdc.shape[3]
     
-    for i in arange(6):
-        pl.pdc_plot(mpdc[i,:,:,:nf/2], sample_f = 500/2, power = False)
+    for i in plot_estag-1:
+        pl.pdc_plot(mpdc[i,:,:,:plot_freq])
         
     pp.show()
     
-    print time.clock() - tim
-    
-    
+    print 'tempo gasto:', time.clock() - tim
     
     #savetxt('C:/Documents and Settings/Stein/Desktop/teste edu/baccala2001pdc.txt', pdcan)
     #savetxt('C:/Documents and Settings/Stein/Desktop/teste edu/baccala2001coh.txt', cohan)
     
-    return pdcan, mpdc
+    return result, mpdc
     
 def testa_aic():
     
